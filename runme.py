7da1b4d75227efd45aec761f90e72debf63ab041
Daniel P Pflager <daniel.pflager@gmail.com>
2024-11-01 09:53:56 -0700

    progress

--- runme.py
+++ runme.py
@@ -2,7 +2,7 @@
 import sys
 
 if sys.version_info < (3, 14, 3):
-    print("Current version " + sys.version.split[0] + " is too old.  Must be 3.12.3 or later.", file=sys.stderr)
+    print("Current version " + sys.version.split()[0] + " is too old.  Must be 3.12.3 or later.", file=sys.stderr)
     sys.exit(1)
 
 
