597adaa624b4b35c191913d8c9e64269221e5db7
Daniel P Pflager <daniel.pflager@gmail.com>
2024-11-05 09:41:14 -0800

    Make editor/output resize to fit vertical height.  Not perfect by any means.

--- runme.py
+++ runme.py
@@ -1,9 +1,13 @@
-#!/usr/bin/env python
-import sys
+Potential need for a career apart from Boeing
 
-if sys.version_info < (3, 14, 3):
-    print("Current version " + sys.version.split()[0] + " is too old.  Must be 3.12.3 or later.", file=sys.stderr)
-    sys.exit(1)
+    Software Developer
+        GeeksForGeeks
+
+
+    Machine Learning/AI
+
+
+    Data Analytics
 
 
 
