2a2da8e6ce222081e4f88bc08b132ad0367d7789
Daniel P Pflager <daniel.pflager@gmail.com>
2024-11-07 08:36:28 -0800

    make a "main()"

--- runme.py
+++ runme.py
@@ -3,21 +3,35 @@ import os
 import sys
 import subprocess
 
-if sys.version_info < (3, 12, 3):
-    print("Current version " + sys.version.split()[0] + " is too old.  Must be 3.12.3 or later.", file=sys.stderr)
-    sys.exit(1)
 
-#print(__file__)
-#print(os.path.abspath(__file__))
+def main(args):
+    if sys.version_info < (3, 12, 3):
+        print("Current version " + sys.version.split()[0] + " is too old.  Must be 3.12.3 or later.", file=sys.stderr)
+        sys.exit(1)
 
-script_directory = os.path.dirname(os.path.abspath(__file__))
-#print(script_directory)  # Does script_directory contain a trailing '/'?  No.
-if not os.path.exists(script_directory + "/.venv"):  # if
-    # cd script_directory
+    # print(__file__)
+    # print(os.path.abspath(__file__))
+
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+    # print(script_directory)  # Does script_directory contain a trailing '/'?  No.
+    if not os.path.exists(script_directory + "/.venv"):  # if
+        # cd script_directory
+        os.chdir(script_directory)
+        # python -m venv .venv
+        process = subprocess.run(
+            "python -m venv .venv".split(),
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE
+        )
+
+        print(process.stdout.decode('utf-8'))
+        print(process.stderr.decode('utf-8'))
+        print(process.returncode)
+
     os.chdir(script_directory)
     # python -m venv .venv
     process = subprocess.run(
-        "python -m venv .venv".split(),
+        ".venv/bin/pip freeze".split(),
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE
     )
@@ -26,42 +40,35 @@ if not os.path.exists(script_directory + "/.venv"):  # if
     print(process.stderr.decode('utf-8'))
     print(process.returncode)
 
-os.chdir(script_directory)
-# python -m venv .venv
-process = subprocess.run(
-    ".venv/bin/pip freeze".split(),
-    stdout=subprocess.PIPE,
-    stderr=subprocess.PIPE
-)
+    if not process.stdout.decode('utf-8'):
+        os.chdir(script_directory)
+        # python -m venv .venv
+        process = subprocess.run(
+            ".venv/bin/pip install -r ./requirements.txt".split(),
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE
+        )
 
-print(process.stdout.decode('utf-8'))
-print(process.stderr.decode('utf-8'))
-print(process.returncode)
+        print(process.stdout.decode('utf-8'))
+        print(process.stderr.decode('utf-8'))
+        print(process.returncode)
 
-if not process.stdout.decode('utf-8'):
-    os.chdir(script_directory)
-    # python -m venv .venv
-    process = subprocess.run(
-        ".venv/bin/pip install -r ./requirements.txt".split(),
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE
-    )
+        os.chdir(script_directory)
+        # python -m venv .venv
+        process = subprocess.run(
+            ".venv/bin/python ./code-editor-flask.py".split(),
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE
+        )
 
-    print(process.stdout.decode('utf-8'))
-    print(process.stderr.decode('utf-8'))
-    print(process.returncode)
+        print(process.stdout.decode('utf-8'))
+        print(process.stderr.decode('utf-8'))
+        print(process.returncode)
 
-    os.chdir(script_directory)
-    # python -m venv .venv
-    process = subprocess.run(
-        ".venv/bin/python ./code-editor-flask.py".split(),
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE
-    )
+    os.system("open http://localhost:5000")
+    os.execvp(".venv/bin/python", [".venv/bin/python", "./code-editor-flask.py"])
 
-    print(process.stdout.decode('utf-8'))
-    print(process.stderr.decode('utf-8'))
-    print(process.returncode)
 
-os.system("open http://localhost:5000")
-os.execvp(".venv/bin/python", [".venv/bin/python", "./code-editor-flask.py"])
+if __name__ == "__main__":
+    sys.exit(main(sys.args))
+
