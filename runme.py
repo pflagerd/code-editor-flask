32c887b9fac4a60b920f1cfa793998ab0acd66d5
Daniel P Pflager <daniel.pflager@gmail.com>
2024-11-07 08:48:50 -0800

    problem in spawn()

--- runme.py
+++ runme.py
@@ -4,6 +4,19 @@ import sys
 import subprocess
 
 
+def spawn(command_line):
+    process = subprocess.run(
+        command_line.split(),
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    )
+
+    return {'stdout': process.stdout.decode('utf-8'),
+            'stderr': process.stderr.decode('utf-8'),
+            'returncode': process.returncode
+            }
+
+
 def main(args):
     if sys.version_info < (3, 12, 3):
         print("Current version " + sys.version.split()[0] + " is too old.  Must be 3.12.3 or later.", file=sys.stderr)
@@ -17,28 +30,30 @@ def main(args):
     if not os.path.exists(script_directory + "/.venv"):  # if
         # cd script_directory
         os.chdir(script_directory)
-        # python -m venv .venv
-        process = subprocess.run(
-            "python -m venv .venv".split(),
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE
-        )
-
-        print(process.stdout.decode('utf-8'))
-        print(process.stderr.decode('utf-8'))
-        print(process.returncode)
+        # # python -m venv .venv
+        # process = subprocess.run(
+        #     "python -m venv .venv".split(),
+        #     stdout=subprocess.PIPE,
+        #     stderr=subprocess.PIPE
+        # )
+        #
+        # print(process.stdout.decode('utf-8'))
+        # print(process.stderr.decode('utf-8'))
+        # print(process.returncode)
+        print(str(spawn("python -m venv .venv")))
 
     os.chdir(script_directory)
-    # python -m venv .venv
-    process = subprocess.run(
-        ".venv/bin/pip freeze".split(),
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE
-    )
-
-    print(process.stdout.decode('utf-8'))
-    print(process.stderr.decode('utf-8'))
-    print(process.returncode)
+    print(str(spawn(".venv/bin/pip freeze")))
+    # # python -m venv .venv
+    # process = subprocess.run(
+    #     ".venv/bin/pip freeze".split(),
+    #     stdout=subprocess.PIPE,
+    #     stderr=subprocess.PIPE
+    # )
+    #
+    # print(process.stdout.decode('utf-8'))
+    # print(process.stderr.decode('utf-8'))
+    # print(process.returncode)
 
     if not process.stdout.decode('utf-8'):
         os.chdir(script_directory)
