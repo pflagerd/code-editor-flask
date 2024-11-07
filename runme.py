b1e15fad69b16cf4e5e18ce2d057bd674202446b
Daniel P Pflager <daniel.pflager@gmail.com>
2024-11-07 09:23:04 -0800

    Create opportunities for printing debug data.

--- runme.py
+++ runme.py
@@ -20,7 +20,7 @@ def spawn(command_line):
     )
 
 
-def main(args):
+def main(args, debug=False):
     if sys.version_info < (3, 12, 3):
         print("Current version " + sys.version.split()[0] + " is too old.  Must be 3.12.3 or later.", file=sys.stderr)
         sys.exit(1)
@@ -33,14 +33,17 @@ def main(args):
     # print(script_directory)  # Does script_directory contain a trailing '/'?  No.
     if not os.path.exists(script_directory + "/.venv"):  # if
         spawn_result = spawn("python -m venv .venv")
-        print(spawn_result)
+        if debug:
+            print(spawn_result)
 
     stdout = spawn(".venv/bin/pip freeze").stdout
-    # print(f'stdout == {stdout}')
+    if debug:
+        print(f'stdout == {stdout}')
 
     if not stdout:
         spawn_result = spawn(".venv/bin/pip install -r requirements.txt")
-        print(spawn_result)
+        if debug:
+            print(spawn_result)
 
     os.system("open http://localhost:5000")
     # time.sleep(5)  # browser appears even to wait long enough if I introduce an artifical pause.
