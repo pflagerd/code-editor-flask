8434a1624fd464e12cc2a9dbed698bdb975cba1e
Daniel P Pflager <oy753c@debian12-netinst-ootb.pflager.net>
2024-12-06 08:57:26 -0800

    update .gitignore and add build-python.bash

---
+++ build-python.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if ! [ -f ./Python-3.12.8.tgz ]; then
+	echo wget https://www.python.org/ftp/python/3.12.8/Python-3.12.8.tgz
+	wget https://www.python.org/ftp/python/3.12.8/Python-3.12.8.tgz
+fi
+
+if ! [ -d Python-3.12.8 ]; then
+	echo tar xf Python-3.12.8.tgz
+	tar xf Python-3.12.8.tgz
+fi
+
+sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev libbz2-dev liblzma-dev tk-dev uuid-dev
+
+cd Python-3.12.8
+
+./configure --prefix=/home/oy753c/code-editor-flask
+
