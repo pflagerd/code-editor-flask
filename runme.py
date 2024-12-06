34c12a033852438b69c8a2b2b2c182ce35fc3185
Daniel P Pflager <daniel.pflager@gmail.com>
2024-12-06 09:30:54 -0800

    merge build-python.bash with RUNME

--- runme.py
+++ runme.py
@@ -7,28 +7,8 @@ import time
 from types import SimpleNamespace
 
 
-def if_kubuntu_package_not_installed_install_it_now(package):
-    if not is_kubuntu():
-        return
-
-    # is package installed?
-    # Do something like this bash command: sudo apt list --installed | grep python3-venv3
-    if package in spawn("sudo apt list --installed").stdout:
-        return
-
-    response = spawn(f"sudo apt install {package} -y")
-    if response.returncode:
-        raise RuntimeError("Something went wrong: " + response.stderr)
-
-def is_kubuntu():
-    if not os.path.exists("/etc/os-release"):
-        return False
-
-    return "Ubuntu" in open("/etc/os-release").read()
-
-
 def main(args, debug=False):
-    supported_python_versions = [(3, 11, 2), (3, 12, 3), (3, 13, 0)]
+    supported_python_versions = [(3, 11, 2), (3, 12, 3), (3, 12, 8), (3, 13, 0)]
     if (sys.version_info.major, sys.version_info.minor, sys.version_info.micro) not in supported_python_versions:
         print("Current version " + sys.version.split()[0] + " not tested.  Must be one of " + version_info_tuple_to_str(supported_python_versions), file=sys.stderr)
         sys.exit(1)
@@ -42,22 +22,12 @@ def main(args, debug=False):
     if debug:
         print(script_directory)  # Does script_directory contain a trailing '/'?  No.
 
-    if_kubuntu_package_not_installed_install_it_now("python3-pip")
-    if_kubuntu_package_not_installed_install_it_now("python3-venv")
-
-    if not os.path.exists(script_directory + "/.venv"):  # if
-        spawn_result = spawn("python3 -m venv .venv")
-        if spawn_result.returncode:
-            raise RuntimeError("python3 -m venv .venv failed: " + spawn_result.stderr)
-        if debug:
-            print(spawn_result)
-
-    stdout = spawn(".venv/bin/pip freeze").stdout
+    stdout = spawn("./bin/pip3 freeze").stdout
     if debug:
         print(f'stdout == {stdout}')
 
     if not stdout:
-        spawn_result = spawn(".venv/bin/pip install -r requirements.txt")
+        spawn_result = spawn("bin/pip3 install -r requirements.txt")
         if debug:
             print(spawn_result)
 
@@ -70,7 +40,7 @@ def main(args, debug=False):
     else:
         print('Neither open nor gio is available in your PATH, cannot launch default browser', file=sys.stderr)
 
-    os.execvp(".venv/bin/python3", [".venv/bin/python3", "./code-editor-flask.py"])
+    os.execvp("bin/python3", ["bin/python3", "./code-editor-flask.py"])
 
 def spawn(command_line):
     process = subprocess.run(
