8309a216e440266051afde252c11c6739405b281
Daniel P Pflager <daniel.pflager@gmail.com>
2024-12-10 20:37:40 -0800

    progress

--- code-editor-flask.py
+++ code-editor-flask.py
@@ -1,10 +1,17 @@
+from contextlib import contextmanager
+import ctypes
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
+import io
+import os
 import sys
-from io import StringIO
+import tempfile
 
 app = Flask(__name__)
 CORS(app)
+
+libc = ctypes.CDLL(None)
+c_stdout = ctypes.c_void_p.in_dll(libc, 'stdout')
 
 @app.route('/')
 def index():
@@ -13,16 +20,49 @@ def index():
 @app.route('/run', methods=['POST'])
 def run_code():
     code = request.json['code']
-    old_stdout = sys.stdout
-    redirected_output = sys.stdout = StringIO()
     
+    f = io.BytesIO()
+
     try:
-        exec(code)
-        sys.stdout = old_stdout
-        return jsonify({'output': redirected_output.getvalue()})
+        with stdout_redirector(f):
+            exec(code)
+
+        return jsonify({'output': f.getvalue().decode('utf-8')})
     except Exception as e:
-        sys.stdout = old_stdout
         return jsonify({'output': str(e)})
+
+@contextmanager
+def stdout_redirector(stream):
+    # The original fd stdout points to. Usually 1 on POSIX systems.
+    original_stdout_fd = sys.stdout.fileno()
+
+    def _redirect_stdout(to_fd):
+        """Redirect stdout to the given file descriptor."""
+        # Flush the C-level buffer stdout
+        libc.fflush(c_stdout)
+        # Flush and close sys.stdout - also closes the file descriptor (fd)
+        sys.stdout.close()
+        # Make original_stdout_fd point to the same file as to_fd
+        os.dup2(to_fd, original_stdout_fd)
+        # Create a new sys.stdout that points to the redirected fd
+        sys.stdout = io.TextIOWrapper(os.fdopen(original_stdout_fd, 'wb'))
+
+    # Save a copy of the original stdout fd in saved_stdout_fd
+    saved_stdout_fd = os.dup(original_stdout_fd)
+    try:
+        # Create a temporary file and redirect stdout to it
+        tfile = tempfile.TemporaryFile(mode='w+b')
+        _redirect_stdout(tfile.fileno())
+        # Yield to caller, then redirect stdout back to the saved fd
+        yield
+        _redirect_stdout(saved_stdout_fd)
+        # Copy contents of temporary file to the given stream
+        tfile.flush()
+        tfile.seek(0, io.SEEK_SET)
+        stream.write(tfile.read())
+    finally:
+        tfile.close()
+        os.close(saved_stdout_fd)
 
 if __name__ == '__main__':
     app.run(debug=True)
