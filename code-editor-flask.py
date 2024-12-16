a19c6826a721dfaa18fbbd383fdd79aeeb151d39
Daniel P Pflager <daniel.pflager@gmail.com>
2024-12-16 09:54:03 -0800

    output still appears out of order when mixing print() with os.system('ls')

--- code-editor-flask.py
+++ code-editor-flask.py
@@ -1,17 +1,12 @@
-from contextlib import contextmanager
-import ctypes
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
-import io
 import os
 import sys
+import subprocess
 import tempfile
 
 app = Flask(__name__)
 CORS(app)
-
-libc = ctypes.CDLL(None)
-c_stdout = ctypes.c_void_p.in_dll(libc, 'stdout')
 
 @app.route('/')
 def index():
@@ -20,49 +15,27 @@ def index():
 @app.route('/run', methods=['POST'])
 def run_code():
     code = request.json['code']
+
+    with tempfile.NamedTemporaryFile() as temp:
+        temp.write(code.encode('utf-8'))
+        temp.flush()
     
-    f = io.BytesIO()
+        try:
+            process = subprocess.Popen(
+                ["python3", temp.name],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+            )
 
-    try:
-        with stdout_redirector(f):
-            exec(code)
+            stdout, stderr = process.communicate()
 
-        return jsonify({'output': f.getvalue().decode('utf-8')})
-    except Exception as e:
-        return jsonify({'output': str(e)})
+            print(stdout.decode('utf-8'))
+            print(stderr.decode('utf-8'))
+            print(process.returncode)
 
-@contextmanager
-def stdout_redirector(stream):
-    # The original fd stdout points to. Usually 1 on POSIX systems.
-    original_stdout_fd = sys.stdout.fileno()
-
-    def _redirect_stdout(to_fd):
-        """Redirect stdout to the given file descriptor."""
-        # Flush the C-level buffer stdout
-        libc.fflush(c_stdout)
-        # Flush and close sys.stdout - also closes the file descriptor (fd)
-        sys.stdout.close()
-        # Make original_stdout_fd point to the same file as to_fd
-        os.dup2(to_fd, original_stdout_fd)
-        # Create a new sys.stdout that points to the redirected fd
-        sys.stdout = io.TextIOWrapper(os.fdopen(original_stdout_fd, 'wb'))
-
-    # Save a copy of the original stdout fd in saved_stdout_fd
-    saved_stdout_fd = os.dup(original_stdout_fd)
-    try:
-        # Create a temporary file and redirect stdout to it
-        tfile = tempfile.TemporaryFile(mode='w+b')
-        _redirect_stdout(tfile.fileno())
-        # Yield to caller, then redirect stdout back to the saved fd
-        yield
-        _redirect_stdout(saved_stdout_fd)
-        # Copy contents of temporary file to the given stream
-        tfile.flush()
-        tfile.seek(0, io.SEEK_SET)
-        stream.write(tfile.read())
-    finally:
-        tfile.close()
-        os.close(saved_stdout_fd)
+            return jsonify({'output': stdout.decode('utf-8')})
+        except Exception as e:
+            return jsonify({'output': str(e)})
 
 if __name__ == '__main__':
     app.run(debug=True)
